0de33a3f3972d0975748499380cf125b916ddd51
Vinathi-Karumuri <vinathikarumuri@gmail.com>
2026-03-02 18:48:36 +0530

    reset link

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ def forgot_password():
         cur.close()
         conn.close()
 
-        reset_link = f"https://vinathee.pythonanywhere.com/reset-password/{token}"
+        reset_link = f"https://Vinathee.pythonanywhere.com/reset-password/{token}"
         send_reset_email(email, reset_link) # (email integration later)
 
         flash("Password reset link sent to your email.", "success")
